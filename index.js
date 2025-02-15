c50fa8e6308b9ec3fa5278bd4dc80e6cf7a27d44
thecosmicsahil <cosmicsahil@gmail.com>
2025-02-15 20:59:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://www.sahilarya.shop/",
+    origin: "https://www.sahilarya.shop",
     methods: ["GET", "POST"]
   }
 });
